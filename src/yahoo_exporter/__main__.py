be6585f28740a4f17ede8f8c63c341b0f5ea48f6
Adrien GIVRY <contact@adrien-givry.com>
2022-06-29 11:03:04 -0400

    Fixing success message with yahoo-exporter

--- src/yahoo_exporter/__main__.py
+++ src/yahoo_exporter/__main__.py
@@ -16,7 +16,7 @@ def main():
 	args = parser.parse_args()
 	document = Document(args.input)
 	export(document, args.output)
-	print("Success! %s created")
+	print("Success! %s created" % args.output)
 
 if __name__ == '__main__':
 	main()
